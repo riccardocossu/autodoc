afcbb77e2463b5ca03f6182768d8490b8bcbe414
riccardocossu.net <riccardo.cossu@gmail.com>
2014-02-20 15:35:54 +0100

    sonatype deploy is finally ok

--- autodoc-validation-plugin/deploy-staging.sh
+++ autodoc-validation-plugin/deploy-staging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mvn clean source:jar javadoc:jar package
+mvn clean package
 mvn -e gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/autodoc-validation-plugin-"$1".jar
 mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/autodoc-validation-plugin-"$1"-javadoc.jar
 mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=pom.xml -Dfile=target/autodoc-validation-plugin-"$1"-sources.jar
